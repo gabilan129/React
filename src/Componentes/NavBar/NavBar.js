11abc37ce1fab920a0cce7bab135222c53934371
gabriel carranza <gabrielraul16@gmail.com>
2023-04-18 19:04:26 -0300

    after final instalo js click

--- src/Componentes/NavBar/NavBar.js
+++ src/Componentes/NavBar/NavBar.js
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react'
 import CardWidget from '../CardWidget/CardWidget'
 import './NavBar.css'
 import LogoPowaTienda from './assets/LogoPowaTienda.png'
 import { Link } from 'react-router-dom'
+import {collection, getDocs, orderBy, query} from "firebase/firestore"
+import {db} from "../../Servicio/firebase/fireBaseConfig"
+
 const NavBar = ()=> {
 
+    const [categorias,setCategorias] = useState([])
 
 
+
+useEffect(()=>{
+
+const categoriaRef = query(collection(db,"categorias"),orderBy("orden","desc"))
+
+getDocs(categoriaRef)
+.then(snapshot=>{
+    const categoriasAdap = snapshot.docs.map(doc =>{
+        const data = doc.data()
+        return { id:doc.id , ...data}
+    })
+    setCategorias(categoriasAdap)
+})
+},[])
+
+console.log(categorias)
 return (
 <header>
     
@@ -13,11 +34,15 @@ return (
          <Link to="/"><img className='logo' src={LogoPowaTienda} alt="logo tienda"/></Link>
 
         <div className='divNavegacion'>
-         
-        <Link className='Link' to="/categoria/celular">Celulares</Link>
+         {
+            categorias.map(cate => {
+                return   <Link key={cate.id} className='Link' to={`/categoria/${cate.slug}`}>{cate.label}</Link>
+            })
+         }
+        {/* <Link className='Link' to="/categoria/celular">Celulares</Link>
         <Link className='Link' to="/categoria/tablet" >Tablet</Link>
         <Link className='Link' to="/categoria/notebook" >notebook</Link>
-        
+         */}
         </div>        
         <CardWidget/>
         
